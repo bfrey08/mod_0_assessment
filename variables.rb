55fc20479895152352bcc55d5aee408d0ddee46c
Billy Frey <bfrey16@gmail.com>
2021-07-22 16:55:05 -0400

    Add part II program specific challenge

---
+++ variables.rb
@@ -0,0 +1,17 @@
+famous_person = "Matt Damon"
+
+is_active = true
+
+famous_person = "John Stewart"
+
+num_3 = 3
+
+num_9 = 9
+
+sum = num_3 + num_9
+
+animals = ["cow", "horse", "turtle"]
+
+animals.shift
+
+p famous_person.length
