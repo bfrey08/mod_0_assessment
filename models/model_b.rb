3b7d1dc073a95bab060bc60b1702d650d19554e2
Billy Frey <bfrey16@gmail.com>
2021-07-22 17:27:22 -0400

    Add oscar cat data

---
+++ models/model_b.rb
@@ -0,0 +1,12 @@
+oscar_cat = {
+  weight_kg:  10.5,
+  height_in:  14,
+  nicknames: ["jimmy", "catdog"],
+  favorite_foods: ["pizza", "hotdogs"],
+  is_hungry: true,
+  is_happy: true,
+  color: "black",
+  breed: "Burmese",
+  name: "Oscar"
+
+}
