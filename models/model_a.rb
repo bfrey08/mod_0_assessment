7e7e64bfb0649c27a7127d4b5b7a23f726f3bcbf
Billy Frey <bfrey16@gmail.com>
2021-07-22 17:15:50 -0400

    Add hard knocks school data

---
+++ models/model_a.rb
@@ -0,0 +1,12 @@
+school_of_hard_knocks = {
+  classes: 235,
+  students: 1500,
+  max_gpa: 4.0,
+  min_gpa: 1.0,
+  superintendent: "Johny Knoxville",
+  principal: "Bo Burnham",
+  is_active: true,
+  is_accredited: false,
+  counelours: ["Jimmy Chin", "Conrad Anker"],
+  mascots: ["Muffin Man", "Mickey Mouse"]
+}
